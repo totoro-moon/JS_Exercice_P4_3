adedf65825d180be0364b70ad609420396dddab6
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-13 13:36:22 +0200

    JS_Exercice_P4_3

---
+++ main.js
@@ -0,0 +1,13 @@
+// variable clic qui débute à 0
+var clicks = 0;
+// fonction qui a chaque clic rajoute 1
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+
+
+
+
+
